5f4d35b8ead6d55056ca00e346b2a2f184a98dcb
LiangWeiHeng1 <740550267@qq.com>
2026-09-17 16:22:27 +0800

    Create MainActivity.java

---
+++ app/src/main/java/com/zhuanhuan/app/MainActivity.java
@@ -0,0 +1,40 @@
+package com.zhuanhuan.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setAllowFileAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // 加载你的 GitHub Pages 网页
+        webView.loadUrl("https://liangweiheng1.github.io/zhuanhuan/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
